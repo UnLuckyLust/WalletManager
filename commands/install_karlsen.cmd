fde8005b5f1c7af4267df1eb14412c8e40e84e7f
UnLuckyLust <unluckylust@gmail.com>
2024-02-28 16:04:14 +0200

    Update install_karlsen.cmd

--- commands/install_karlsen.cmd
+++ commands/install_karlsen.cmd
@@ -1,4 +1,4 @@
-@REM Written by UnLuckyLust - https://github.com/UnLuckyLust/KarlsenControl
+@REM Written by UnLuckyLust - https://github.com/UnLuckyLust/WalletManager
 @echo off
 setlocal
 cd /d %~dp0
